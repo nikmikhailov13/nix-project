2fa68def19f58cd553e2643b6156ce5af1bb8c04
Nikita Mykhailov <nikita.mykhailov@adevinta.com>
2023-01-14 09:56:04 +0100

    add photo

---
+++ src/main/resources/db/changelog/004-create-table-photo.sql
@@ -0,0 +1,6 @@
+CREATE TABLE nikitamykhailov.nix.photo(
+                          id BIGINT NOT NULL PRIMARY KEY,
+                          url VARCHAR(255) NOT NULL,
+                          student_id BIGINT NOT NULL,
+                          FOREIGN KEY (student_id) REFERENCES nikitamykhailov.nix.student(id)
+);
